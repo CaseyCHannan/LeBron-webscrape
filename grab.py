4f75a7e76018ac384f64fc08dad5e18713974aa2
Casey Hannan <Hannan.Casey@gmail.com>
2020-02-01 12:47:21 -0500

    added ordinality. Print statements more correct / concise

--- grab.py
+++ grab.py
@@ -1,8 +1,9 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import pandas as pd
+import inflect
+p = inflect.engine()
 
-# URL pages we will scraping (see image above)
 
 def who_next(url, stat ):
     html = urlopen(url)
@@ -16,20 +17,16 @@ def who_next(url, stat ):
     bron_outer =brontag.find_parent('tr')
     # print(bron_outer)
     bron_inner = bron_outer.find('td')
-    bron_stat = bron_outer.find_all('td')[-1].string
-    print(f'I am {bron_outer.a.string}')
-    print(f'I have {bron_stat} {stat}\n')
-
+    bron_stat = int(bron_outer.find_all('td')[-1].string)
+    
     next_up_outer = bron_outer.find_previous_sibling('tr')
     next_up_name = next_up_outer.a.string
-    next_up_stat = next_up_outer.contents[-1].string
-    print(f'I am {next_up_name}')
-    print(f'i have {next_up_stat} {stat} \n')
+    next_up_stat = int(next_up_outer.contents[-1].string)
+    stat_diff = next_up_stat - bron_stat + 1
+    bron_place = int(bron_inner.contents[0].rstrip('.')) 
+    next_up_ord = p.ordinal(bron_place - 1) #zero-index
+    print(f'LeBron James has {bron_stat} career {stat}')
+    print(f"he needs {stat_diff} {stat}")
+    print(f'to surpass {next_up_name}\'s {next_up_stat} {stat} for {next_up_ord} place all time\n')
 
-    #next_up_inner = next_up_outer.find('td')
-    
-    bron_id = int(bron_inner.contents[0].rstrip('.'))
-    bron_data_row= (bron_id - 1) #zero-indexed
-
-    print(int(bron_id))
     return
