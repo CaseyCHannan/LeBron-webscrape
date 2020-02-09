80398e8488c878c6c4333bd367991a595542930f
Casey Hannan <Hannan.Casey@gmail.com>
2020-02-08 21:28:12 -0500

    styling and icons added

--- hello.py
+++ hello.py
@@ -1,12 +1,15 @@
 from flask import Flask, escape, render_template, request, redirect
 from grab import who_next 
-from main import stats
+from main import stats, teams, icons
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return render_template("index.html", stats=stats)
+    return render_template("index.html", stats=stats, teams=teams, icons= icons)
 
+@app.route('/about')
+def about():
+    return render_template("about.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from grab import who_next 
 
+
 url_points= "https://www.basketball-reference.com/leaders/pts_career.html"
 url_assists = "https://www.basketball-reference.com/leaders/ast_career.html"
 url_rebounds = "https://www.basketball-reference.com/leaders/trb_career.html"
@@ -7,14 +8,9 @@ url_field_goals ="https://www.basketball-reference.com/leaders/fg_career.html"
 url_win_shares = "https://www.basketball-reference.com/leaders/ws_career.html"
 
 # this is the HTML from the given URL
-
-# who_next(url_points, 'points')
-# who_next(url_assists, 'assists')
-# who_next(url_rebounds, 'rebounds')
-# who_next(url_field_goals, 'field goals')
-# who_next(url_win_shares, 'winshares')
-
-
+icons = ['sidehoop','hoop_rock', 'bball_flames','bball_trophy', 'crown','bball_trophy',
+    'ball','hoop_rock' ]
+teams = ['CLE', 'MIA', 'CLE', 'LAL']
 stats = []
 
 points = who_next(url_points,"points")
